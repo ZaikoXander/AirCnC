3319f2fb74497a889935ce4e2d1904f3c750ed1e
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-05-15 03:08:35 -0300

    feat: created Booking Controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import multer from "multer"
 import uploadConfig from "./config/upload.js"
-import DashboardController from "./controllers/DashboardController.js"
 
 import SessionController from "./controllers/SessionController.js"
 import SpotController from "./controllers/SpotController.js"
+import DashboardController from "./controllers/DashboardController.js"
+import BookingController from "./controllers/BookingController.js"
 
 const routes = Router()
 const upload = multer(uploadConfig)
@@ -15,5 +16,7 @@ routes.get("/spots", SpotController.index)
 routes.post("/spots", upload.single("thumbnail"), SpotController.store)
 
 routes.get("/dashboard", DashboardController.show)
+
+routes.post('/spots/:spot_id/bookings', BookingController.store)
 
 export default routes
