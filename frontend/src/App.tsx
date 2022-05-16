89ddd428180cb2a34b96817d137dac190f76b12b
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-05-16 17:02:13 -0300

    feat(api): created a connection between the frontend and the backend. fix(api): CORS Policy was not allowing api communication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
-import './App.css'
+import { useState } from "react"
+import api from "./services/api"
+
+import "./App.css"
 
 import logo from "./assets/logo.svg"
 
 function App() {
+  const [email, setEmail] = useState('')
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const response = await api.post("/sessions", { email })
+
+    const { _id } = response.data
+
+    console.log(_id)
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
@@ -10,9 +25,16 @@ function App() {
         <p>
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
-          <input type="email" name="email" id="email" placeholder='Seu melhor e-mail' />
+          <input 
+            type="email"
+            name="email"
+            id="email"
+            placeholder='Seu melhor e-mail'
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
           <button className="btn" type="submit">Entrar</button>
         </form>
       </div>
